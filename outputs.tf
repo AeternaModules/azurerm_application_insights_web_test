2c4a6e3ec3e8f567d9e6ccca9a9d000a02436b7f
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,61 +1,61 @@
 output "application_insights_web_tests_id" {
   description = "Map of id values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.id }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "application_insights_web_tests_application_insights_id" {
   description = "Map of application_insights_id values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.application_insights_id }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.application_insights_id if v.application_insights_id != null && length(v.application_insights_id) > 0 }
 }
 output "application_insights_web_tests_configuration" {
   description = "Map of configuration values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.configuration }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.configuration if v.configuration != null && length(v.configuration) > 0 }
 }
 output "application_insights_web_tests_description" {
   description = "Map of description values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.description }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "application_insights_web_tests_enabled" {
   description = "Map of enabled values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.enabled }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.enabled if v.enabled != null }
 }
 output "application_insights_web_tests_frequency" {
   description = "Map of frequency values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.frequency }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.frequency if v.frequency != null }
 }
 output "application_insights_web_tests_geo_locations" {
   description = "Map of geo_locations values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.geo_locations }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.geo_locations if v.geo_locations != null && length(v.geo_locations) > 0 }
 }
 output "application_insights_web_tests_kind" {
   description = "Map of kind values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.kind }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.kind if v.kind != null && length(v.kind) > 0 }
 }
 output "application_insights_web_tests_location" {
   description = "Map of location values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.location }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "application_insights_web_tests_name" {
   description = "Map of name values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.name }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "application_insights_web_tests_resource_group_name" {
   description = "Map of resource_group_name values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.resource_group_name }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "application_insights_web_tests_retry_enabled" {
   description = "Map of retry_enabled values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.retry_enabled }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.retry_enabled if v.retry_enabled != null }
 }
 output "application_insights_web_tests_synthetic_monitor_id" {
   description = "Map of synthetic_monitor_id values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.synthetic_monitor_id }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.synthetic_monitor_id if v.synthetic_monitor_id != null && length(v.synthetic_monitor_id) > 0 }
 }
 output "application_insights_web_tests_tags" {
   description = "Map of tags values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.tags }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 output "application_insights_web_tests_timeout" {
   description = "Map of timeout values across all application_insights_web_tests, keyed the same as var.application_insights_web_tests"
-  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.timeout }
+  value       = { for k, v in azurerm_application_insights_web_test.application_insights_web_tests : k => v.timeout if v.timeout != null }
 }
 
